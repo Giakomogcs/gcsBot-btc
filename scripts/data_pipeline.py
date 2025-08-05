832334ef968bd5f96dc631e19c3a45b6908fbe41
Giovani Costa Silva <giakomogcs@gmail.com>
2025-08-05 15:40:59 -0300

    .

--- scripts/data_pipeline.py
+++ scripts/data_pipeline.py
@@ -707,6 +707,7 @@ class DataPipeline:
             logger.info("✅ A tabela mestre ('features_master_table') já está atualizada. Nenhuma ação necessária.")
             return
         # --- FIM DA LÓGICA DE PROCESSAMENTO INCREMENTAL ---
+        current_date = start_date
         total_months = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month) + 1
         pbar = tqdm(total=total_months, desc="Processando Tabela Mestre")
         
